983a61b9508c3ee0fbaea6b78320a600f852cead
hoang nguyen huu <hoang.nguyenhuu@lotussoft.vn>
2022-03-25 13:11:18 +0700

    [LF] Write first java file for learn basic syntax

---
+++ Learning Fundamentals/Basic Syntax/MyFirstJavaProgram.java
@@ -0,0 +1,9 @@
+
+public class MyFirstJavaProgram{
+    /***
+     * Đây là chương trình java đầu tiên trong project này
+     */
+    public static void main(String args[]){
+        System.out.println("Hello world");
+    }
+}
